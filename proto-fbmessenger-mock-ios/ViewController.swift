a2c99e11c823bb2f770212996c8f87f699db640e
Santosh Krishnamurthy <san.b350@gmail.com>
2023-04-05 14:55:31 -0500

    Initial Commit

---
+++ proto-fbmessenger-mock-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proto-fbmessenger-mock-ios
+//
+//  Created by Santosh Krishnamurthy on 4/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
